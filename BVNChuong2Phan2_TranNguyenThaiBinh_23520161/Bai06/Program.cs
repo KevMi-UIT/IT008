e907bd3262fd5284635e42ddf66d937ecbdce159
Leminionn <137601647+Leminionn@users.noreply.github.com>
2024-09-15 23:13:54 +0700

    feat:  done baitapchuong2phan2 bai06

---
+++ BVNChuong2Phan2_TranNguyenThaiBinh_23520161/Bai06/Program.cs
@@ -0,0 +1,48 @@
+﻿// class in class
+
+namespace Bai06;
+
+public class Person(string _lastName = "", string _firstName = "")
+{
+    public class Date
+    {
+        private int _day;
+        private int _month;
+        private int _year;
+        public Date() { _day = 1; _month = 1; _year = 1; }
+        public Date(int init_day, int init_month, int init_year)
+        {
+            _day = init_day;
+            _month = init_month;
+            _year = init_year;
+        }
+        public void PrintDate()
+        {
+            Console.WriteLine($"{_day}/{_month}/{_year}");
+        }
+    }
+
+    public string FirstName { get; set; } = _firstName;
+    public string LastName { get; set; } = _lastName;
+    private Date Date_of_Birth = new Date();
+    public void setDateOfBirth(int day, int month, int year)
+    {
+        Date_of_Birth = new Date(day, month, year);
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Ho va ten: {LastName} {FirstName}");
+        Console.Write("Ngay sinh: ");
+        Date_of_Birth.PrintDate();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Person a = new Person("Nguyen Van", "B");
+        a.setDateOfBirth(31, 12, 1999);
+        a.Print();
+    }
+}
