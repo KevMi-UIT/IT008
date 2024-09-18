8f46f5df54af46a750bb62eb5df119dbbd4c3169
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-18 08:46:50 +0700

    feat: done bai04

---
+++ BTH1_PhanLeMinh_23520952/BTH1_PhanLeMinh_23520952/Bai04/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Bai04;
+
+public class Program
+{
+    private static readonly int[] DateLeap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    private static readonly int[] DateNoLeap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    private static bool CheckLeap(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    static void Main(string[] args)
+    {
+        int month = int.Parse(Console.ReadLine() ?? "");
+        int year = int.Parse(Console.ReadLine() ?? "");
+        if (CheckLeap(year))
+        {
+            Console.WriteLine($"Nam {year} co {DateLeap[month - 1]} thang");
+        }
+        else
+        {
+            Console.WriteLine($"Nam {year} co {DateNoLeap[month - 1]} thang");
+        }
+
+    }
+}
