9eb0fd982fc4d9e4cc46d8f0c7b43af3dca5f61e
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-18 08:37:34 +0700

    feat: done bai02

--- BTH1_PhanLeMinh_23520952/BTH1_PhanLeMinh_23520952/Bai02/Program.cs
+++ BTH1_PhanLeMinh_23520952/BTH1_PhanLeMinh_23520952/Bai02/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Bai02;
+
+public class Program
+{
+    private static bool CheckPrime(int num)
+    {
+        int lim = (int)Math.Sqrt(num);
+        for (int i = 2; i <= lim; ++i)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Nhap vao n: ");
+        if (!int.TryParse(Console.ReadLine(), out int n))
+        {
+            Console.WriteLine("Loi input!");
+            return;
+        }
+
+        int res = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            if (CheckPrime(i))
+            {
+                Console.WriteLine(i);
+                res += i;
+            }
+        }
+
+        Console.WriteLine($"Tong so nguyen to < {n}: {res}");
+    }
+}
