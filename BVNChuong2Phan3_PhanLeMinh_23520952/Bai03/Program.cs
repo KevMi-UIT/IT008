0117653908c47b38fadd859282cfa5b6bafe6595
Leminionn <137601647+Leminionn@users.noreply.github.com>
2024-09-21 17:52:16 +0700

    feat: up bai 01, 03, 05

--- BVNChuong2Phan3_PhanLeMinh_23520952/Bai03/Program.cs
+++ BVNChuong2Phan3_PhanLeMinh_23520952/Bai03/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 153
+
+namespace Bai03;
+delegate void FunctionToCall(ref int x);
+delegate void MethodDelegateToCall(ref int x);
+public class AddValue
+{
+    public static void Add2(ref int x)
+    {
+        x += 2;
+    }
+
+    public static void Add3(ref int x)
+    {
+        x += 3;
+    }
+    public static void Result(ref int x)
+    {
+        Console.WriteLine("Hello Delegate");
+        Console.WriteLine($"Gia tri cua x thay doi boi delegate: {x}");
+    }
+}
+
+public class Program
+{
+
+    static void Main()
+    {
+        {
+            Console.Write("Nhap gia tri cua x: ");
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out int x))
+            {
+                // Khai báo đồng thời gán delegate bằng Add2 từ lớp AddValue
+                FunctionToCall functionDelegate = AddValue.Add2;
+                functionDelegate += AddValue.Add3;
+                functionDelegate += AddValue.Add2;
+                functionDelegate += AddValue.Add2;
+                functionDelegate(ref x); // Gọi delegate 
+                MethodDelegateToCall CallResultByDelegate = (ref int num) => AddValue.Result(ref num);
+                CallResultByDelegate(ref x); // Gọi method từ lớp khác thông qua delegate;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
+
