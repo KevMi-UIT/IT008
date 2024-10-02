27fb6b9250b8406138ef4488e6c6dcf47c003b22
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-10-02 08:11:29 +0700

    feat(BT2): create

---
+++ BT2_TranNguyenThaiBinh_23520161/Bai01/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bai01;
+
+class Program
+{
+    static int year = new();
+    static int month = new();
+    static readonly int[,] calendar = new int[6, 7];
+    private static DateTime date;
+
+    static void Main()
+    {
+        Console.Write("Enter the year? ");
+        year = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Enter the month (January = 1, etc): ");
+        month = int.Parse(Console.ReadLine() ?? "0");
+
+        date = new DateTime(year, month, 1);
+        DrawHeader();
+        FillCalendar();
+        DrawCalendar();
+        Console.ReadLine();
+    }
+
+    static void DrawHeader()
+    {
+        Console.WriteLine("");
+        Console.WriteLine(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month) + " " + year);
+        Console.WriteLine("Mo Tu We Th Fr Sa Su");
+    }
+
+    static void FillCalendar()
+    {
+        int days = DateTime.DaysInMonth(year, month);
+        int currentDay = 1;
+        var dayOfWeek = (int)date.DayOfWeek;
+        for (int i = 0; i < calendar.GetLength(0); i++)
+        {
+            for (int j = 0; j < calendar.GetLength(1) && currentDay - dayOfWeek + 1 <= days; j++)
+            {
+                if (i == 0 && month > j)
+                {
+                    calendar[i, j] = 0;
+                }
+                else
+                {
+                    calendar[i, j] = currentDay - dayOfWeek + 1;
+                    currentDay++;
+                }
+            }
+        }
+    }
+
+    static void DrawCalendar()
+    {
+        for (int i = 0; i < calendar.GetLength(0); i++)
+        {
+            for (int j = 0; j < calendar.GetLength(1); j++)
+            {
+                if (calendar[i, j] > 0)
+                {
+                    if (calendar[i, j] < 10)
+                    {
+                        Console.Write(" " + calendar[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(calendar[i, j] + " ");
+                    }
+                }
+                else
+                {
+                    Console.Write("   ");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
